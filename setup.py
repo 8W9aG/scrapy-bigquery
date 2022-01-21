4e9a4aa044570aebe1aaa7301e0790e5a6264443
William Sackfield <will.sackfield@gmail.com>
2022-01-20 19:38:34 -0600

    Upgrade to 1.0.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='scrapy-bigquery',
-    version='1.0.5',
+    version='1.0.6',
     description='Scrapy pipeline to store items into BigQuery',
     long_description=long_description,
     long_description_content_type='text/markdown',
